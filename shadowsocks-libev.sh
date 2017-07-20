18f93ec1376ad1b53d0136f53cc9cd4012ac636c
bangbang93 <bangbang93@163.com>
2017-07-20 16:16:39 +0800

    漏了submodule

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -1,7 +1,10 @@
+F=$1
 yum install epel-release -y
 yum install git gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto udns-devel libev-devel libsodium-devel mbedtls-devel -y
+cd ${F}
 git clone https://github.com/shadowsocks/shadowsocks-libev.git
 cd shadowsocks-libev
+git submodule update --init --recursive
 ./autogen.sh
 ./configure
 CORE=`grep "model name" /proc/cpuinfo|wc -l`
