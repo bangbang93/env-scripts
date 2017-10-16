5e30c2ef03c51f2afcfd786c4927e51694c46415
bangbang93 <bangbang93@163.com>
2017-10-16 09:44:40 +0800

    tempdir

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -1,6 +1,10 @@
 F=$1
+if [ !$F ]; then
+  F=/tmp
+fi
 sudo yum install epel-release -y
 sudo yum install git gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto udns-devel libev-devel libsodium-devel mbedtls-devel -y
+echo cloning into $F
 cd ${F}
 git clone https://github.com/shadowsocks/shadowsocks-libev.git
 cd shadowsocks-libev
